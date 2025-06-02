f6a63429723fd23e70eadfa43c0035ca1fbefa98
Joe <joe@showblender.com>
2025-06-01 19:14:53 -0700

    Initial Commit

---
+++ InfinityBug/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InfinityBug
+//
+//  Created by Joseph McCraw on 6/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
